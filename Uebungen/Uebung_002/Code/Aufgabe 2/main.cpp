819803d3729aaece6cba8434a1604d985579445f
codingiwithmagga <codingwithmagga@gmail.com>
2019-10-06 14:03:18 +0200

    Add Uebung 2

---
+++ Uebungen/Uebung_002/Code/Aufgabe 2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    vector<double> vec;
+    vec.push_back(2.0);
+
+    pair<string, double> one_pair("hallo", 3.0);
+
+    cout << "End of programm" << endl;
+
+    return 0;
+}
