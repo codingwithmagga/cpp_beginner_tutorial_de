aad2d312516207c4c0bbaa77d99537fdd5f4da58
codingiwithmagga <codingwithmagga@gmail.com>
2019-12-21 16:19:21 +0100

    add Code for Tut7

---
+++ Tutorials/Tutorial_007/Code/Tutorial_007/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+    //   Gebe Hello World aus
+    std::cout << "Hello World!" << std::endl;
+
+    double a = 1.0;
+    double b = 1.0;
+    double c = 2.0;
+
+    // Berechne das Produkt aus b und c und weiße es a zu
+    a = b * c;
+
+    // Berechne Norm des Vektors v
+    /*
+     * normberchnung des Vektors v
+     */
+
+    // Bubble Sort algorithmus, sortiert vektor v aufsteigend (<), bei gleichgroßen
+    // Elementen (==) bleibt die Reihenfolge erhalten
+    /*
+     * code Zeilen zum Bubble Sort algorithmus
+     */
+
+    return 0;
+}
